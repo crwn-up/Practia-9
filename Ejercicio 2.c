58527c991ae672c8ab197ec9fd36188db0ce16ed
crwn-up <luidran1@gmail.com>
2025-05-08 14:23:15 -0600

    Update Ejercicio 2.c

--- Ejercicio 2.c
+++ Ejercicio 2.c
@@ -1,21 +1,19 @@
+// Ejercicio 2
+// Luis Crown
 #include <stdio.h>
 
 int main() {
     float calificaciones[5];
-    float suma = 0.0;
+    float suma = 0;
     int i;
 
-    // Capturar las 5 calificaciones
     for (i = 0; i < 5; i++) {
         printf("Ingresa la calificación %d: ", i + 1);
         scanf("%f", &calificaciones[i]);
         suma += calificaciones[i];
     }
-
-    // Calcular el promedio
     float promedio = suma / 5;
 
-    // Imprimir el promedio
     printf("El promedio del alumno es: %.2f\n", promedio);
 
     return 0;
