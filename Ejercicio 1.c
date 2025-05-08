423170619b2aa6ecd18ae033e92ddb3365a5e8f3
crwn-up <luidran1@gmail.com>
2025-05-08 13:57:03 -0600

    Create Ejercicio 1.c

---
+++ Ejercicio 1.c
@@ -0,0 +1,20 @@
+// Dado una lista con 5 enteros, imprimir la lista 
+// Práctica 9
+// Ejercicio 1
+
+#include <stdio.h>
+void main ()
+{
+    int lista[5] = {57,73,24,17,20};
+    int i;
+    
+    printf("Los numero de la lista son:");
+    
+    for (i=0; i<5; i++)
+    {
+    
+    printf("%d,", lista [i]);
+    }
+    printf("\n");
+    }
+
