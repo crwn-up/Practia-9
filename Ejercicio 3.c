3facf7eda26eaa9e42f9cefdac7b54ba13815640
crwn-up <luidran1@gmail.com>
2025-05-08 14:51:18 -0600

    Create Ejercicio 3.c

---
+++ Ejercicio 3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int suma(int a, int b)
+{
+        return (a+b);
+        
+}
+void main()
+{
+    int res, a = 10, b =6;
+    
+    res = suma (a,b);
+    printf("La suma es %d\n", res);
+}
+
+
+
+
+#include <stdio.h>
+
+int suma(int a, int b)
+void main()
+{
+        int res, a = 10, b =6;
+        res = suma (a,b);
+        printf("La suma es %d\n", res);
+}
+int suma (int a, int b)
+{
+      return (a+b);
+}
